3fbda4a81ccb301d0a0a897664b2bd2f7a966226
VPO <wennylsonn@gmail.com>
2025-05-04 11:39:12 -0300

    fix

--- model/event.class.js
+++ model/event.class.js
@@ -54,21 +54,19 @@ export default class Event {
           $sort: { "_id": 1 }
         }
       ]);
-
-      console.log(result)
   
       return result.map(day => ({
-        date: moment.utc(day._id).tz('America/Sao_Paulo').format('DD/MM'),
-        day_of_week: moment.utc(day._id).tz('America/Sao_Paulo').format('dddd'),
+        date: moment(day._id).format('DD/MM'),
+        day_of_week: moment(day._id).format('dddd'),
         spaces: day.spaces
           .sort((a, b) => a.name.localeCompare(b.name)) // ordena pelo nome
           .map(space => ({
             name: space.name,
             activities: space.activities.map(activity => ({
               _id: activity._id,
-              time: moment.utc(activity.date).tz('America/Sao_Paulo').format('HH:mm'),
+              time: moment(activity.date).format('HH:mm'),
               title: activity.title,
-              date: moment.utc(activity.date).tz('America/Sao_Paulo').toDate()
+              date: moment(activity.date).toDate()
             }))
           }))
       }));
